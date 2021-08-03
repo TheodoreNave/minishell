588690cd4681b39113b866b2740eceeeb98a7cbe
Théodore Nave <theodoreyws@hotmail.fr>
2021-08-03 15:58:40 +0200

    minishell base

---
+++ includes/minishell.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnave <tnave@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/03 15:56:31 by tnave             #+#    #+#             */
+/*   Updated: 2021/08/03 15:58:23 by tnave            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <errno.h>
+
+typedef	struct s_shell
+{
+} t_shell;
+
+
+#endif
