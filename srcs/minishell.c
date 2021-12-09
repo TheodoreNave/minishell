3b2002caae1fa70e0fe7cff546052a5820d62ec7
Theo <tnave@Theos-iMac.local>
2021-12-09 02:50:48 +0100

    started pipex

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: tnave <tnave@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/03 15:55:32 by tnave             #+#    #+#             */
-/*   Updated: 2021/12/09 02:02:23 by tnave            ###   ########.fr       */
+/*   Updated: 2021/12/09 02:49:06 by tnave            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void	signals(int sig)
 	{
 		write(1, "\n", 1);
 		rl_on_new_line();
-		rl_replace_line("", 0);
+		// rl_replace_line("", 0);
 		rl_redisplay();
 	}
 }
@@ -46,6 +46,72 @@ void	mem(t_utils *utils, t_shell *shell)
 	ft_memset(shell, 0, sizeof(t_shell));
 }
 
+void	opt_exec_mini(char **environ, t_utils *utils, t_utils_list *tmp)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid < 0)
+		ft_error(0, strerror(errno), utils);
+	if (pid == 0)
+	{
+		if (!tmp->prev)
+			dup2(utils->fd_one, STDIN);
+		else
+			dup2(tmp->prev->pfd[STDIN], STDIN);
+		if (tmp->next)
+			dup2(tmp->pfd[STDOUT], STDOUT);
+		else
+			dup2(utils->fd_two, STDOUT);
+		execve(tmp->path, tmp->cmd_opt, environ);
+		exit(127);
+	}
+	else
+		child(pid, utils, tmp);
+}
+
+void		suite_pipex(t_utils *utils, char **environ)
+{
+	t_utils_list *tmp;
+
+	tmp = utils->lst;
+	while (tmp)
+	{
+		if (tmp->next)
+		{
+			if (pipe(tmp->pfd) == -1)
+				ft_error(0, strerror(errno), utils);
+		}
+		opt_exec_mini(environ, utils, tmp);
+		tmp = tmp->next;
+	}
+}
+
+void		ft_check_access_mini(char **environ, int i, t_shell *shell, t_utils *utils)
+{
+	while (utils->parse_env && utils->parse_env[i])
+	{
+		utils->join = ft_strjoin_three(utils->parse_env[i],
+			"/", shell->action->opt[0]);
+			printf("========\n");
+		printf("[%s]\n", utils->join);
+			printf("========\n");
+		if (access(utils->join, F_OK) == 0)
+		{
+			printf("cool\n");
+			ft_lstadd_back(&utils->lst, ft_lstnew(shell->action->opt,
+				ft_strdup(utils->join)));
+		}
+		else
+		{
+			printf("PAS BON LA COMMANDE\n");
+			// break;
+		}
+		i++;
+	}
+	suite_pipex(utils, environ);
+}
+
 int main(int ac, char **av, char **env)
 {
     t_utils utils;
@@ -53,6 +119,7 @@ int main(int ac, char **av, char **env)
 
 	(void)ac;
 	(void)av;
+	int i = 0;
 	mem(&utils, &shell);
 	if (chdir(getenv("HOME")) == -1)
 		ft_error_two("chdir()", &shell, 1);
@@ -71,7 +138,13 @@ int main(int ac, char **av, char **env)
 			fill_cmd(&shell);
 
 		parse_env_minishell(env, &utils);
-
+		// if (pipe)
+		while (i <= 3)	// while tant que env existe encore
+		{
+			ft_check_access_mini(env, i, &shell, &utils);	// 2 commandes ou plus
+			i++;
+		}
+		// if (!pipe)
 		if (shell.action)
 		{
 			if (!built_in_check(shell.action->opt, &shell))
