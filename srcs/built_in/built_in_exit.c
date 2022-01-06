63f30d4be6bef39dbaf0f5eb0dd316f5d5ec62e6
theodorenave <theodorenave@github.com>
2022-01-06 18:15:10 +0100

    added

--- srcs/built_in/built_in_exit.c
+++ srcs/built_in/built_in_exit.c
@@ -6,7 +6,7 @@
 /*   By: tnave <tnave@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 01:55:47 by tnave             #+#    #+#             */
-/*   Updated: 2022/01/06 17:34:35 by tnave            ###   ########.fr       */
+/*   Updated: 2022/01/06 18:14:38 by tnave            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,21 @@
 
 int	built_in_exit(t_shell *shell)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
+	// Arguments replace 0 par opt[1];
 	if (shell->environ)
 		ft_lstclear_env(&shell->environ);
 	if (shell->action)
 		ft_lstclear_action(&shell->action);
 	if (shell->dol)
 		ft_lstclear_dol(&shell->dol);
-	printf("exit\n");
 	if (i > 255)
-		i = i % 256;
+		exit(128);
 	else if (i < 0)
-		i = i % 256;
-	exit(i);
+		exit(128);
+	printf("exit\n");
+	exit(5);
 	// calcul a faire en fonction de l'exit si non compries entre 0 et 255 sinon renvoyer valeur entre 0 et 255
 }
