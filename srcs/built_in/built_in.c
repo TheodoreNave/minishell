b872b57b77c65bbdcd21d97c2e443e4d74a9f95f
theodorenave <theodorenave@github.com>
2022-01-22 17:17:33 +0100

    added

--- srcs/built_in/built_in.c
+++ srcs/built_in/built_in.c
@@ -6,7 +6,7 @@
 /*   By: tnave <tnave@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 10:44:39 by tnave             #+#    #+#             */
-/*   Updated: 2022/01/19 18:04:20 by tnave            ###   ########.fr       */
+/*   Updated: 2022/01/22 17:16:08 by tnave            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	built_in_check(char **built_in, t_shell *shell)
 {
 	if (built_in[0])
 	{
-		if (!strcmp(built_in[0], "cd"))
+		if (!ft_strcmp(built_in[0], "cd"))
 			built_in_cd(shell, built_in);
 		else if (!ft_strcmp(built_in[0], "pwd"))
 			built_in_pwd(shell);
